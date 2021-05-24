6b76be6329089a1d8895e95b37bd92ceb139991c
Mark Cracknell <mark@marksweb.org.uk>
2021-05-24 15:08:05 +0100

    write function uses paylaod identical to cloud Battery API

--- write.py
+++ write.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import sys
+import json
 from GivTCP import GivTCP
 from GivLUT import GiV_Reg_LUT
 
@@ -9,7 +10,7 @@ def setChargeEnable(control):
     else:
        val=1
     result=GivTCP.write_single_register(96,val)
-    print ("Setting Charge Enable was: ",result)
+    return ("Setting Charge Enable was: ",result)
 
 def setDischargeEnable(control):
     if control=="off":
@@ -17,37 +18,51 @@ def setDischargeEnable(control):
     else:
        val=1
     result=GivTCP.write_single_register(59,val)
-    print ("Setting Discharge Enable was: ",result)
+    return ("Setting Discharge Enable was: ",result)
 
-def setChargeSlot1(start,end,target):
+def setChargeSlot1(payload):
+    params=json.loads(payload)
+    start=params['start']
+    end=params['finish']
+    target=params['chargeToPercent']
     startresult=GivTCP.write_single_register(94,start)
     endresult=GivTCP.write_single_register(95,end)
     targetresult=GivTCP.write_single_register(116,target)
     enableresult=GivTCP.write_single_register(96,1)     #enable charge flag automatically
     if startresult=="Success" and endresult=="Success" and targetresult=="Success" and enableresult=="Success":
-        print ("Charge Time successfully set")
+        return ("Charge Time successfully set")
     else:
-        print ("Error setting Charge time")
+        return ("Error setting Charge time")
 
-def setDishargeSlot1(start,end,target):
+def setDishargeSlot1(payload):
+    params=json.loads(payload)
+    start=params['start']
+    end=params['finish']
+    target=params['dischargeToPercent']
     startresult=GivTCP.write_single_register(56,start)
     endresult=GivTCP.write_single_register(57,end)
     targetresult=GivTCP.write_single_register(114,target)
     if startresult=="Success" and endresult=="Success" and targetresult=="Success":
-        print ("Charge enable successfully set")
+        return ("Charge enable successfully set")
     else:
-        print ("Error setting Discharge Slot 1 times")
+        return ("Error setting Discharge Slot 1 times")
 
-def setDishargeSlot2(start,end,target):
+def setDishargeSlot2(payload):
+    params=json.loads(payload)
+    start=params['start']
+    end=params['finish']
+    target=params['dischargeToPercent']
     startresult=GivTCP.write_single_register(44,start)
     endresult=GivTCP.write_single_register(45,end)
     targetresult=GivTCP.write_single_register(114,end)
     if startresult=="Success" and endresult=="Success" and targetresult=="Success":
-        print ("Disharge TImeslot 2 successfully set")
+        return ("Disharge TImeslot 2 successfully set")
     else:
-        print ("Error setting Discharge Slot 2 times")
+        return ("Error setting Discharge Slot 2 times")
 
-def setBatteryMode(mode):
+def setBatteryMode(payload):
+    params=json.loads(payload)
+    start=params['mode']
     if mode==1:
         shallowresult=GivTCP.write_single_register(110,4)
         dischargeresult=GivTCP.write_single_register(59,0)
@@ -69,9 +84,9 @@ def setBatteryMode(mode):
 
     #Calculate success
     if shallowresult=="Success" and dischargeresult=="Success" and selfresult=="Success":
-        print ("Control Mode successfully set")
+        return ("Control Mode successfully set")
     else:
-        print ("Error setting Control Mode")
+        return ("Error setting Control Mode")
 
 
 if __name__ == '__main__':
