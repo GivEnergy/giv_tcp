592492db33f9234181ef4ec66586afac3030f9b8
root <root@a0d7b954-ssh.local.hass.io>
2021-06-05 11:35:50 +0100

    Test function for Simon

--- read.py
+++ read.py
@@ -25,7 +25,7 @@ def getTimeslots():
     timeslots.update(GivTCP.read_register('56','03','02'))
     timeslots.update(GivTCP.read_register('94','03','02'))
     if len(timeslots)!=0:
-      GivTCP.publish_to_MQTT("Timeslots",timeslots)
+        GivTCP.publish_to_MQTT("Timeslots",timeslots)
 
 def getCombinedStats():
     energy_total_output={}
@@ -47,7 +47,7 @@ def getCombinedStats():
     if Print_Raw:
         GivTCP.publish_to_MQTT("raw/input",temp_output)
 
-    extrareg=GivTCP.read_register('180','04','4') #Get ALL input Registers
+    extrareg=GivTCP.read_register('180','04','4') #Get v2.6 input Registers
     if len(extrareg)==4:
         if extrareg[GiV_Reg_LUT.input_register_LUT.get(183)[0]+"(183)"]<100:	#Cap output at 100kWh in a single day
             energy_today_output['Battery Charge Energy Today kWh']=extrareg[GiV_Reg_LUT.input_register_LUT.get(183)[0]+"(183)"]
@@ -98,7 +98,7 @@ def getCombinedStats():
         temphex=str(temp_output[GiV_Reg_LUT.input_register_LUT.get(45)[0]+"(45)"])+str(temp_output[GiV_Reg_LUT.input_register_LUT.get(46)[0]+"(46)"])
         invout_kwh_value=round(int(temphex,16) * GiV_Reg_LUT.input_register_LUT.get(45)[2],2)
 
-        #Energy Today Fugures
+        #Energy Today Figures
         energy_today_output['Battery Throughput Today kWh']=temp_output[GiV_Reg_LUT.input_register_LUT.get(36)[0]+"(36)"]+temp_output[GiV_Reg_LUT.input_register_LUT.get(37)[0]+"(37)"]
         energy_today_output['PV Energy Today kWh']=round(temp_output[GiV_Reg_LUT.input_register_LUT.get(17)[0]+"(17)"]+temp_output[GiV_Reg_LUT.input_register_LUT.get(19)[0]+"(19)"],2)
         if temp_output[GiV_Reg_LUT.input_register_LUT.get(26)[0]+"(26)"]<100:
@@ -193,13 +193,13 @@ def getCombinedStats():
 
         #Publish to MQTT
         if len(energy_total_output)!=0:
-          GivTCP.publish_to_MQTT("Energy/Total",energy_total_output)
+            GivTCP.publish_to_MQTT("Energy/Total",energy_total_output)
         if len(energy_today_output)!=0:
-          GivTCP.publish_to_MQTT("Energy/Today",energy_today_output)
+            GivTCP.publish_to_MQTT("Energy/Today",energy_today_output)
         if len(power_output)!=0:
-          GivTCP.publish_to_MQTT("Power",power_output)
+            GivTCP.publish_to_MQTT("Power",power_output)
         if len(power_flow_output)!=0:
-          GivTCP.publish_to_MQTT("Power/Flows",power_flow_output)
+            GivTCP.publish_to_MQTT("Power/Flows",power_flow_output)
 
 def getModes():
     controls={}
@@ -210,43 +210,56 @@ def getModes():
         GivTCP.publish_to_MQTT("raw/holding",controls)
 
     if len(controls)==120:
-      shallow_charge=controls[GiV_Reg_LUT.holding_register_LUT.get(110)[0]+"(110)"]
-      self_consumption=controls[GiV_Reg_LUT.holding_register_LUT.get(27)[0]+"(27)"]
-      charge_enable=controls[GiV_Reg_LUT.holding_register_LUT.get(96)[0]+"(96)"]
-      if charge_enable==True:
-          charge_enable="Active"
-      else:
-          charge_enable="Paused"
+        shallow_charge=controls[GiV_Reg_LUT.holding_register_LUT.get(110)[0]+"(110)"]
+        self_consumption=controls[GiV_Reg_LUT.holding_register_LUT.get(27)[0]+"(27)"]
+        charge_enable=controls[GiV_Reg_LUT.holding_register_LUT.get(96)[0]+"(96)"]
+        if charge_enable==True:
+            charge_enable="Active"
+        else:
+            charge_enable="Paused"
 
-      battery_reserve=controls[GiV_Reg_LUT.holding_register_LUT.get(114)[0]+"(114)"]
-      target_soc=controls[GiV_Reg_LUT.holding_register_LUT.get(116)[0]+"(116)"]
-      battery_capacity=controls[GiV_Reg_LUT.holding_register_LUT.get(55)[0]+"(55)"]
-      discharge_enable=controls[GiV_Reg_LUT.holding_register_LUT.get(59)[0]+"(59)"]
-      if discharge_enable==True:
-          discharge_enable="Active"
-      else:
-          discharge_enable="Paused"
+        battery_reserve=controls[GiV_Reg_LUT.holding_register_LUT.get(114)[0]+"(114)"]
+        target_soc=controls[GiV_Reg_LUT.holding_register_LUT.get(116)[0]+"(116)"]
+        battery_capacity=controls[GiV_Reg_LUT.holding_register_LUT.get(55)[0]+"(55)"]
+        discharge_enable=controls[GiV_Reg_LUT.holding_register_LUT.get(59)[0]+"(59)"]
+        if discharge_enable==True:
+            discharge_enable="Active"
+        else:
+            discharge_enable="Paused"
 
-      print (shallow_charge,self_consumption,discharge_enable)
+        print (shallow_charge,self_consumption,discharge_enable)
 
-      if shallow_charge==4 and self_consumption==True and discharge_enable=="Paused":
-          mode=1
-      elif shallow_charge==100 and self_consumption==True and discharge_enable=="Active":
-          mode=3
-      elif shallow_charge==4 and self_consumption==False and discharge_enable=="Active":
-          mode=4
-      else:
-          mode="unknown"
+        if shallow_charge==4 and self_consumption==True and discharge_enable=="Paused":
+            mode=1
+        elif shallow_charge==100 and self_consumption==True and discharge_enable=="Active":
+            mode=3
+        elif shallow_charge==4 and self_consumption==False and discharge_enable=="Active":
+            mode=4
+        else:
+            mode="unknown"
 
-      controlmode['Mode']=mode
-      controlmode['Battery Power Reserve']=battery_reserve
-      controlmode['Target SOC']=target_soc
-      controlmode['Battery Capacity']=round(((battery_capacity*51.2)/1000),2)
-      controlmode['Charge Schedule State']=charge_enable
-      controlmode['Discharge Schedule State']=discharge_enable
+        controlmode['Mode']=mode
+        controlmode['Battery Power Reserve']=battery_reserve
+        controlmode['Target SOC']=target_soc
+        controlmode['Battery Capacity']=round(((battery_capacity*51.2)/1000),2)
+        controlmode['Charge Schedule State']=charge_enable
+        controlmode['Discharge Schedule State']=discharge_enable
 
     if len(controlmode)!=0:
-      GivTCP.publish_to_MQTT("Control",controlmode)
+        GivTCP.publish_to_MQTT("Control",controlmode)
+
+def extraRegCheck():
+    extrareg={}
+    extrareg=GivTCP.read_register('180','04','4') #Get v2.6 input Registers
+    print ("Extrareg is:",extrareg)
+    print ("Extrareg is:",len(extrareg),"registers long")
+    if len(extrareg)==4:
+        if extrareg[GiV_Reg_LUT.input_register_LUT.get(183)[0]+"(183)"]<100:	#Cap output at 100kWh in a single day
+            energy_today_output['Battery Charge Energy Today kWh']=extrareg[GiV_Reg_LUT.input_register_LUT.get(183)[0]+"(183)"]
+        if extrareg[GiV_Reg_LUT.input_register_LUT.get(182)[0]+"(182)"]<100:
+            energy_today_output['Battery Discharge Energy Today kWh']=extrareg[GiV_Reg_LUT.input_register_LUT.get(182)[0]+"(182)"]
+        energy_total_output['Battery Charge Energy Total kWh']=extrareg[GiV_Reg_LUT.input_register_LUT.get(181)[0]+"(181)"]
+        energy_total_output['Battery Discharge Energy Total kWh']=extrareg[GiV_Reg_LUT.input_register_LUT.get(180)[0]+"(180)"]
 
 def runAll():
     getCombinedStats()
