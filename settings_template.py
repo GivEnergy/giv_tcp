dd27ec5f2f13df8f8f71153ba7e4c021b85ff9de
root <root@a0d7b954-ssh.local.hass.io>
2021-06-12 21:12:48 +0100

    Adding in JSON output option

--- settings_template.py
+++ settings_template.py
@@ -1,11 +1,11 @@
 class GiV_Settings:
   invertorIP=""           #Required - IP address of Invertor on local network
   dataloggerSN=""         #Required - Wifi/GPS S/N found here: https://www.givenergy.cloud/GivManage/setting/deviceMenu/inverterList
-  MQTT_Address=""         #Required - IP address of MQTT broker (local or remote)
+  output=""               #Required - "MQTT" or "JSON"
+  MQTT_Address=""         #Required - if output is MQTT IP address of MQTT broker (local or remote)
   MQTT_Username=""        #Optional - Username for MQTT broker
   MQTT_Password=""        #Optional - Password for MQTT broker
-  MQTT_Topic=""           #Optional - Root topic for all MQTT messages. Defaults to "GivEnergy/<dataloggerSN>
-  Log_To_File=""          #Optional - When used with debug this outputs logging to file 
-  Debug_File_Location=""  #Optional - When used with Log_To_File this will write to specified location (Default is root directory)
+  MQTT_Topic=""           #Optional - Root topic for all MQTT messages. Defaults to "GivEnergy/<dataloggerSN> 
+  debug=""                #Optional - Enables verbose debug "True" or "False".
+  Debug_File_Location=""  #Optional - Location of debug file (Default is root directory)
   Print_Raw_Registers=""  #Optional - "True" prints all raw registers to the MQTT broker
-  debug=""                #Optional - Enables verbose debug "True" or "False". Default location is stdout
