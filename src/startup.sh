7d1881f4db9a5f3599034fb325283e25ff8c6cd6
root <root@a0d7b954-ssh.local.hass.io>
2021-06-22 19:54:15 +0100

    AD fix

--- src/startup.sh
+++ src/startup.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
-#Only used for Docker Version
+#Only used for Docker Deployment
 
 rm settings.py	# remove settings.py in case it exists
 
 printf "class GiV_Settings:\n" >> settings.py
-if [$INVERTOR_IP=""]
-then
-    outputString=$(python3 /app/findInvertor.py)
-    printf "    invertorIP=\"$outputString\"\n" >> settings.py
-else
-    printf "    invertorIP=\"$INVERTOR_IP\"\n" >> settings.py
-fi
+#if [$INVERTOR_IP=""]
+#then
+outputString=$(python3 findInvertor.py)
+printf "    invertorIP=\"$outputString\"\n" >> settings.py
+#else
+#    printf "    invertorIP=\"$INVERTOR_IP\"\n" >> settings.py
+#fi
 printf "    dataloggerSN=\"$SERIAL_NUMBER\"\n" >> settings.py
 printf "    Print_Raw_Registers=\"$PRINT_RAW\"\n" >> settings.py
 printf "    MQTT_Address=\"$MQTT_ADDRESS\"\n" >> settings.py
@@ -21,4 +21,4 @@ printf "    output=\"$OUTPUT\"\n" >> settings.py
 printf "    debug=\"$DEBUG\"\n" >> settings.py
 printf "    Debug_File_Location=\"$DEBUG_FILE_LOCATION\"\n" >> settings.py
 
-gunicorn -w 3 -b :6345 REST:giv_api
+#gunicorn -w 3 -b :6345 REST:giv_api
