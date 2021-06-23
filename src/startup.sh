ce05fd32f61ffddf0b4092823317d7a367fef4e7
root <root@a0d7b954-ssh.local.hass.io>
2021-06-23 12:08:58 +0100

    AutoDiscovery update

--- src/startup.sh
+++ src/startup.sh
@@ -8,13 +8,14 @@ if [ $INVERTOR_IP='' ]
 then
     echo 'IP not set in ENV, scanning network...'
     outputString=`python3 findInvertor.py`
-    if [ outputString='' ]
+    if [ -z "$outputString" ]
     then
         echo 'No invertor found... Please add into ENV manually'
         exit 1
     else
-        echo 'Invertor found at $outputString'
+        echo Invertor found at "$outputString"
         printf "    invertorIP=\"$outputString\"\n" >> settings.py
+    fi
 else
     printf "    invertorIP=\"$INVERTOR_IP\"\n" >> settings.py
 fi
@@ -27,4 +28,5 @@ printf "    output=\"$OUTPUT\"\n" >> settings.py
 printf "    debug=\"$DEBUG\"\n" >> settings.py
 printf "    Debug_File_Location=\"$DEBUG_FILE_LOCATION\"\n" >> settings.py
 
+echo 'Starting Gunicorn on port 6345'
 gunicorn -w 3 -b :6345 REST:giv_api
