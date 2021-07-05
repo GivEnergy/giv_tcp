ff7f4a9956ffff4cfe025fb6810f0d4152a20339
root <root@a0d7b954-ssh.local.hass.io>
2021-07-05 22:10:22 +0100

    HA publishing capability

--- src/HomeAssist.py
+++ src/HomeAssist.py
@@ -1,20 +1,35 @@
-import json
-import sys
-from homeassistant_api import Client
 from GivTCP import GivTCP
 from settings import GiV_Settings
+from requests import post
 
-class GivHA():
-    def push(data):
-        url=GiV_Settings.HA_url
-        token=GiV_Settings.HA_token
-        client = Client(url, token)
-        power_output = data['Power']
-        GivTCP.debug("Creating Power entities for HA")
-        for key in power_output:
-            print("inside loop")
+class GivHA:
+    def publish(data):
+        headers = {
+        'Authorization': 'Bearer '+GiV_Settings.HA_token,
+        'content-type': 'application/json',
+        }
+        for key in data:
             entityid="sensor.givtcp_"+key.replace(" ","_")
-            payload='{"state":"'+str(power_output[key])+'"}'
-            print("sending",str(power_output[key]), "to", entityid.lower()) 
-            result=client.set_entity(entity_id='sensor.givtcp_test', state='test')
-            print (result)
+            url = GiV_Settings.HA_url+'/api/states/'+entityid
+            payload='{"state":"'+str(data[key])+'"}'
+            GivTCP.debug("Publishing "+str(entityid)+": "+str(payload))
+            response = post(url, headers=headers, data=payload)
+
+    def push(data):
+        if 'power' in data.keys():
+            power_output = data['Power']
+            GivTCP.debug("Creating Power entities for HA")
+            GivHA.publish(power_output)
+        
+        if 'Energy/Today' in data.keys():
+            energy_today_output = data['Energy/Today']
+            GivTCP.debug("Creating Today Energy entities for HA")
+            GivHA.publish(energy_today_output)
+
+        if 'Energy/Today' in data.keys():
+            energy_total_output = data['Energy/Total']
+            GivTCP.debug("Creating Total Energy entities for HA")
+            GivHA.publish(energy_total_output)
+
+if __name__ == '__main__':
+    globals()[sys.argv[1]]()
