c5c97b42a2d19ab5d1ecd5be1282f1d40ff66a71
britkat <mark@marksweb.org.uk>
2022-02-03 13:04:42 +0000

    buildx update

--- buildx.bat
+++ buildx.bat
@@ -1,2 +1,2 @@
 git pull
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6 -t britkat/giv_tcp-ma:2021.11.24 --push .
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6 -t britkat/giv_tcp-ma:latest --push .
