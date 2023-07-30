fcc9a692373f3432c7134bcb23ee3c7fd22fa4f3
britkat1980 <69121158+britkat1980@users.noreply.github.com>
2023-07-30 14:50:36 +0100

    Update Config trnslations re: isAC

--- buildx.bat
+++ buildx.bat
@@ -1,3 +1,3 @@
-::docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6 -t britkat/giv_tcp-dev:2.2.122 -t britkat/giv_tcp-dev:latest --push .
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6 -t britkat/giv_tcp-dev:2.2.12 -t britkat/giv_tcp-dev:latest --push .
 ::docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6 -t britkat/giv_tcp-dev:latest --push .
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6 -t britkat/giv_tcp-ma:latest -t britkat/giv_tcp-ma:2.3.0 --push .
+::docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6 -t britkat/giv_tcp-ma:latest -t britkat/giv_tcp-ma:2.3.0 --push .
